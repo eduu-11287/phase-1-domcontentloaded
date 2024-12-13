436d0721fc408b154f814d7199c060cc8a2a8fa9
Edwin <edmboche141@gmail.com>
2024-12-13 09:53:26 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+    // Select the paragraph element with the id "text"
+    var paragraph = document.getElementById('text');
+    
+    // Change the text content of the paragraph using textContent
+    paragraph.textContent = "This is really cool!";
+});
